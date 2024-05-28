b2827d23efa5d45634683382fc0f5ba1f51adcdd
Franco Gomez <frangomez0409@gmail.com>
2024-05-27 22:29:10 -0300

    initial commit

---
+++ src/main/java/com/davinci/SpringbootEj2/SpringbootEj2Application.java
@@ -0,0 +1,13 @@
+package com.davinci.SpringbootEj2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootEj2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootEj2Application.class, args);
+	}
+
+}
